8ec181d12d4ec68b2cb8b0785cc29436d66f8545
glocker <georgeglocker@gmail.com>
2020-12-15 04:13:03 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const frameworks = [
+	{id: 1,	title: 'Angular', img: 'https://images.app.goo.gl/E8YQS7yZRzfKeLdB7'},
+	{id: 2, title: 'React', img: 'https://images.app.goo.gl/mqnwsvEesz6iATZG9'},
+	{id: 3, title: 'Vue', img: 'https://images.app.goo.gl/8rHb1ia2KWNtckTR9'},
+	{id: 4, title: 'Svelte', img: 'https://images.app.goo.gl/Z56YW9sdu2mhNMoW9'}
+]
+
+const modal = $.modal({
+	title: 'Modal window title',
+	closable: true,
+	content: `<h4><p>Text</p></h4>`,
+	width: '400px'
+})
